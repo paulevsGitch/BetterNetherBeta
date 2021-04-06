24235d79946b00b782a4f8db462916b59a2fc1a1
paulevsGitch <paulevs@yandex.ru>
2021-04-06 10:37:57 +0300

    Separate particle list for biome particles (wrong particle render fix)

---
+++ src/main/java/paulevs/bnb/mixin/ParticleManagerMixin.java
@@ -0,0 +1,84 @@
+package paulevs.bnb.mixin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.texture.TextureManager;
+import net.minecraft.entity.EntityBase;
+import net.minecraft.entity.ParticleBase;
+import net.minecraft.level.Level;
+import net.minecraft.util.maths.MathHelper;
+import paulevs.bnb.BetterNetherBeta;
+import paulevs.bnb.particles.BiomeParticle;
+
+@Mixin(ParticleManager.class)
+public class ParticleManagerMixin {
+	private static List<ParticleBase> biomeParticles = new ArrayList<ParticleBase>(128);
+	private static int textureID = -1;
+	
+	@Shadow
+	private TextureManager textureManager;
+	
+	@Inject(method = "method_323", at = @At("TAIL"))
+	private void bnb_clearParticles(Level arg, CallbackInfo info) {
+		biomeParticles.clear();
+	}
+	
+	@Inject(method = "addParticle", at = @At("HEAD"), cancellable = true)
+	private void bnb_addParticle(ParticleBase particle, CallbackInfo info) {
+		if (particle instanceof BiomeParticle) {
+			if (biomeParticles.size() > 127) {
+				biomeParticles.remove(0);
+			}
+			biomeParticles.add(particle);
+			info.cancel();
+		}
+	}
+	
+	@Inject(method = "method_320", at = @At("TAIL"))
+	private void bnb_particleTick(CallbackInfo info) {
+		for (int i = 0; i < biomeParticles.size(); i++) {
+			ParticleBase particle = biomeParticles.get(i);
+			particle.tick();
+			if (particle.removed) {
+				biomeParticles.remove(i);
+				i--;
+			}
+		}
+	}
+	
+	@Inject(method = "method_324", at = @At("TAIL"))
+	private void bnb_renderParticles(EntityBase entity, float f, CallbackInfo info) {
+		if (!biomeParticles.isEmpty()) {
+			float yaw = entity.yaw * (float) Math.PI / 180.0F;
+			float pitch = entity.pitch * (float) Math.PI / 180.0F;
+			float yawCos = MathHelper.cos(yaw);
+			float yawSin = MathHelper.sin(yaw);
+			float pitchSin = MathHelper.sin(pitch);
+			float pitchCos = MathHelper.cos(pitch);
+			float var5 = -yawSin * pitchSin;
+			float var6 = yawCos * pitchSin;
+			
+			if (textureID < 0) {
+				textureID = textureManager.getTextureId("/assets/" + BetterNetherBeta.MOD_ID + "/textures/particles.png");
+			}
+			textureManager.bindTexture(textureID);
+			
+			Tessellator tesselator = Tessellator.INSTANCE;
+			tesselator.start();
+			for (int i = 0; i < biomeParticles.size(); i++) {
+				ParticleBase particle = biomeParticles.get(i);
+				particle.method_2002(tesselator, f, yawCos, pitchCos, yawSin, var5, var6);
+			}
+			tesselator.draw();
+		}
+	}
+}
--- src/main/java/paulevs/bnb/particles/BiomeParticle.java
+++ src/main/java/paulevs/bnb/particles/BiomeParticle.java
@@ -1,15 +1,12 @@
 package paulevs.bnb.particles;
 
 import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.texture.TextureManager;
 import net.minecraft.entity.ParticleBase;
 import net.minecraft.level.Level;
-import paulevs.bnb.BetterNetherBeta;
 import paulevs.bnb.util.ClientUtil;
 import paulevs.bnb.util.MHelper;
 
 public class BiomeParticle extends ParticleBase {
-	private static int textureID = -1;
 	private float nextSpeedX;
 	private float nextSpeedY;
 	private float nextSpeedZ;
@@ -24,7 +21,7 @@ public class BiomeParticle extends ParticleBase {
 		super(level, x, y, z, 0, 0, 0);
 		this.setPositionAndAngles(x, y, z, 0, 0);
 		maxAge = MHelper.randRange(100, 300, ClientUtil.getRandom());
-		field_2640 = MHelper.randRange(0.5F, 1.5F, ClientUtil.getRandom()); // Particle size
+		field_2640 = MHelper.randRange(0.75F, 1.5F, ClientUtil.getRandom()); // Particle size
 		field_2635 = index; // Particle texture index
 		randomizeSpeed();
 		velocityX = 0;
@@ -87,25 +84,19 @@ public class BiomeParticle extends ParticleBase {
 	
 	@Override
 	public void method_2002(Tessellator tessellator, float f, float x, float y, float z, float f4, float f5) {
-		TextureManager manager = ClientUtil.getMinecraft().textureManager;
-		if (textureID < 0) {
-			textureID = manager.getTextureId("/assets/" + BetterNetherBeta.MOD_ID + "/textures/particles.png");
-		}
-		manager.bindTexture(textureID);
-		
 		float u1 = (this.field_2635 & 15) / 16F;
 		float u2 = u1 + 0.0624375F;
 		float v1 = (this.field_2635 / 16) / 16.0F;
 		float v2 = v1 + 0.0624375F;
 		float scale = 0.1F * this.field_2640 * alpha;
-		float var13 = (float)(this.prevX + (this.x - this.prevX) * f - field_2645);
-		float var14 = (float)(this.prevY + (this.y - this.prevY) * f - field_2646);
-		float var15 = (float)(this.prevZ + (this.z - this.prevZ) * f - field_2647);
+		float posX = (float)(this.prevX + (this.x - this.prevX) * f - field_2645);
+		float posY = (float)(this.prevY + (this.y - this.prevY) * f - field_2646);
+		float posZ = (float)(this.prevZ + (this.z - this.prevZ) * f - field_2647);
 		float light = this.getBrightnessAtEyes(f);
 		tessellator.colour(this.field_2642 * light, this.field_2643 * light, this.field_2644 * light);
-		tessellator.vertex((var13 - x * scale - f4 * scale), (var14 - y * scale), (var15 - z * scale - f5 * scale), u2, v2);
-		tessellator.vertex((var13 - x * scale + f4 * scale), (var14 + y * scale), (var15 - z * scale + f5 * scale), u2, v1);
-		tessellator.vertex((var13 + x * scale + f4 * scale), (var14 + y * scale), (var15 + z * scale + f5 * scale), u1, v1);
-		tessellator.vertex((var13 + x * scale - f4 * scale), (var14 - y * scale), (var15 + z * scale - f5 * scale), u1, v2);
+		tessellator.vertex((posX - x * scale - f4 * scale), (posY - y * scale), (posZ - z * scale - f5 * scale), u2, v2);
+		tessellator.vertex((posX - x * scale + f4 * scale), (posY + y * scale), (posZ - z * scale + f5 * scale), u2, v1);
+		tessellator.vertex((posX + x * scale + f4 * scale), (posY + y * scale), (posZ + z * scale + f5 * scale), u1, v1);
+		tessellator.vertex((posX + x * scale - f4 * scale), (posY - y * scale), (posZ + z * scale - f5 * scale), u1, v2);
 	}
 }
