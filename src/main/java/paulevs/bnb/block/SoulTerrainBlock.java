f587b78d775d95b68efc8406d62015fd471124d5
paulevsGitch <paulevs@yandex.ru>
2023-06-17 13:27:57 +0300

    Nylium texture update

--- src/main/java/paulevs/bnb/block/SoulTerrainBlock.java
+++ src/main/java/paulevs/bnb/block/SoulTerrainBlock.java
@@ -2,11 +2,21 @@ package paulevs.bnb.block;
 
 import net.minecraft.block.material.Material;
 import net.modificationstation.stationapi.api.registry.Identifier;
+import paulevs.bnb.BNB;
 
 public class SoulTerrainBlock extends NetherTerrainBlock {
 	public SoulTerrainBlock(Identifier id) {
 		super(id, Material.DIRT);
 		setHardness(SOUL_SAND.getHardness());
 		setSounds(SOUL_SAND.sounds);
+	}
+	
+	@Override
+	public Identifier[] getTextureNames(Identifier id) {
+		return new Identifier[] {
+			BNB.id("block/soul_soil"),
+			BNB.id("block/" + id.id + "_top"),
+			BNB.id("block/" + id.id + "_side")
+		};
 	}
 }
