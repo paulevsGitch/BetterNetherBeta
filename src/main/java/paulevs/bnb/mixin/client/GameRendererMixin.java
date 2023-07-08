0de168a846703b5bda46d3b41db5ef7cfc7a18e5
paulevsGitch <paulevs@yandex.ru>
2023-07-08 23:05:19 +0300

    Fixed fog calculations when FarView is installed

--- src/main/java/paulevs/bnb/mixin/client/GameRendererMixin.java
+++ src/main/java/paulevs/bnb/mixin/client/GameRendererMixin.java
@@ -3,6 +3,7 @@ package paulevs.bnb.mixin.client;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.render.GameRenderer;
+import net.modificationstation.stationapi.api.util.math.MathHelper;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,11 +25,11 @@ public class GameRendererMixin {
 	
 	@Inject(method = "setupFog", at = @At("HEAD"))
 	private void bnb_changeFogColor(int i, float par2, CallbackInfo info) {
-		if (this.minecraft.level.dimension.id != -1) return;
+		if (minecraft.level.dimension.id != -1) return;
 		FogInfo.setColor(0.2F, 0.03F, 0.03F);
-		this.fogColorR = FogInfo.COLOR[0];
-		this.fogColorG = FogInfo.COLOR[1];
-		this.fogColorB = FogInfo.COLOR[2];
+		fogColorR = FogInfo.COLOR[0];
+		fogColorG = FogInfo.COLOR[1];
+		fogColorB = FogInfo.COLOR[2];
 	}
 	
 	@Inject(method = "renderFog", at = @At(
@@ -37,7 +38,7 @@ public class GameRendererMixin {
 		shift = Shift.AFTER
 	))
 	private void method_1852(float f, CallbackInfo info) {
-		if (this.minecraft.level.dimension.id != -1) return;
+		if (minecraft.level.dimension.id != -1) return;
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 		GL11.glClearColor(
 			FogInfo.COLOR[0],
@@ -56,11 +57,14 @@ public class GameRendererMixin {
 	private void bnb_changeNetherFog(int i, float par2, CallbackInfo info) {
 		if (this.minecraft.level.dimension.id != -1) return;
 		if (BNB_FARVIEW) {
-			GL11.glFogf(GL11.GL_FOG_START, 64F);
-			GL11.glFogf(GL11.GL_FOG_END, 375F);
+			float delta = minecraft.options.viewDistance / 3F;
+			float fogStart = MathHelper.lerp(delta, 64F, 0F);
+			float fogEnd = MathHelper.lerp(delta, 375F, 64F);
+			GL11.glFogf(GL11.GL_FOG_START, fogStart);
+			GL11.glFogf(GL11.GL_FOG_END, fogEnd);
 			return;
 		}
-		GL11.glFogf(GL11.GL_FOG_START, this.fogDistance * 0.5F);
-		GL11.glFogf(GL11.GL_FOG_END, this.fogDistance);
+		GL11.glFogf(GL11.GL_FOG_START, fogDistance * 0.5F);
+		GL11.glFogf(GL11.GL_FOG_END, fogDistance);
 	}
 }
