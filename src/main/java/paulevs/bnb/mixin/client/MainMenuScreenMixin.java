7127e399098ebbe4b306cdcffef44c040efce02d
paulevsGitch <paulevs@yandex.ru>
2023-08-26 00:58:49 +0300

    Updated to latest StAPI

--- src/main/java/paulevs/bnb/mixin/client/MainMenuScreenMixin.java
+++ src/main/java/paulevs/bnb/mixin/client/MainMenuScreenMixin.java
@@ -1,14 +1,14 @@
 package paulevs.bnb.mixin.client;
 
-import net.minecraft.client.gui.screen.menu.MainMenu;
+import net.minecraft.client.gui.screen.menu.MainMenuScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import paulevs.bnb.sound.BNBSoundManager;
 
-@Mixin(MainMenu.class)
-public class MainMenuMixin {
+@Mixin(MainMenuScreen.class)
+public class MainMenuScreenMixin {
 	@Inject(method = "<init>", at = @At("TAIL"))
 	private void bnb_onInit(CallbackInfo info) {
 		BNBSoundManager.setInTheNether(false);
