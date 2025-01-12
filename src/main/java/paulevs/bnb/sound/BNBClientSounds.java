ccfca98ebe54811eecf41077023c7886ddffc65b
paulevsGitch <paulevs@yandex.ru>
2025-01-12 06:09:30 +0300

    Updated Connor Crow music, removed other music

--- src/main/java/paulevs/bnb/sound/BNBClientSounds.java
+++ src/main/java/paulevs/bnb/sound/BNBClientSounds.java
@@ -29,13 +29,9 @@ public class BNBClientSounds {
 	}
 	
 	private static final SoundEntry[] MUSIC = new SoundEntry[] {
-		getSound(BNB.id("music/faultlines-asher_fulero")),
-		getSound(BNB.id("music/lament_of_the_ancients-asher_fulero")),
-		getSound(BNB.id("music/sinister_cathedral-asher_fulero")),
-		getSound(BNB.id("music/an_excuse_to_do_less_not_more-patches")),
-		getSound(BNB.id("music/those_things_are_more_fun_with_other_people-patches")),
 		getSound(BNB.id("music/conner_crow_phlogiston")),
-		getSound(BNB.id("music/conner_crow_glowstone_lullaby"))
+		getSound(BNB.id("music/conner_crow_glowstone_lullaby")),
+		getSound(BNB.id("music/conner_crow_bravais"))
 	};
 	
 	private static final byte[] MUSIC_INDEX_DATA = new byte[MUSIC.length];
