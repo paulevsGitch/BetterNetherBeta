72c699271271dd13302ef8c3168427f01615d9c1
paulevsGitch <paulevs@yandex.ru>
2025-01-15 02:30:58 +0300

    Fix for wrong spider spawn

--- src/main/java/paulevs/bnb/block/entity/CocoonSpawnerBlockEntity.java
+++ src/main/java/paulevs/bnb/block/entity/CocoonSpawnerBlockEntity.java
@@ -1,12 +1,15 @@
 package paulevs.bnb.block.entity;
 
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityRegistry;
 import net.minecraft.entity.living.LivingEntity;
 import net.minecraft.util.maths.Box;
 import net.minecraft.util.maths.MCMath;
+import net.modificationstation.stationapi.api.block.BlockState;
 import net.modificationstation.stationapi.api.registry.BlockRegistry;
 import net.modificationstation.stationapi.api.util.Identifier;
+import paulevs.bnb.block.BNBBlockTags;
 import paulevs.bnb.entity.NetherSpiderEntity;
 
 import java.util.Random;
@@ -39,7 +42,7 @@ public class CocoonSpawnerBlockEntity extends BlockEntity {
 		for(byte i = 0; i < count; ++i) {
 			LivingEntity spider = (LivingEntity) EntityRegistry.create(entity, level);
 			spider.setPosition(x + random.nextGaussian(), y, z + random.nextGaussian());
-			if (!canSpawn(spider) || level.containsLiquids(spider.boundingBox)) continue;
+			if (!canSpawn(spider)) continue;
 			level.spawnEntity(spider);
 		}
 	}
@@ -49,19 +52,28 @@ public class CocoonSpawnerBlockEntity extends BlockEntity {
 	}
 	
 	private static boolean canSpawn(LivingEntity entity) {
+		if (entity.level.canSuffocate(MCMath.floor(entity.x), MCMath.floor(entity.y), MCMath.floor(entity.z))) return false;
+		if (!entity.level.getEntities(entity.getClass(), entity.boundingBox).isEmpty()) return false;
+		
 		int x1 = MCMath.floor(entity.boundingBox.minX);
 		int y1 = MCMath.floor(entity.boundingBox.minY);
-		int z1 = MCMath.floor(entity.boundingBox.minY);
+		int z1 = MCMath.floor(entity.boundingBox.minZ);
 		int x2 = MCMath.floor(entity.boundingBox.maxX);
 		int y2 = MCMath.floor(entity.boundingBox.maxY);
-		int z2 = MCMath.floor(entity.boundingBox.maxY);
+		int z2 = MCMath.floor(entity.boundingBox.maxZ);
+		
 		for (int x = x1; x <= x2; x++) {
 			for (int y = y1; y <= y2; y++) {
 				for (int z = z1; z <= z2; z++) {
-					if (entity.level.canSuffocate(x, y, z)) return false;
+					BlockState state = entity.level.getBlockState(x, y, z);
+					if (state.isIn(BNBBlockTags.OBSIDIAN)) return false;
+					if (state.getBlock().material == Material.LAVA) return false;
+					boolean canSpawn = state.isAir() || state.getBlock().material.hasNoSuffocation() || !state.getBlock().material.blocksMovement();
+					if (!canSpawn) return false;
 				}
 			}
 		}
+		
 		return true;
 	}
 }
