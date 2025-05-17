cf300367a7ee99b0c0113bf99db5a53ccac10e64
paulevsGitch <paulevs@yandex.ru>
2025-05-17 06:32:20 +0300

    Fixed client-side code in boat mixin

--- src/main/java/paulevs/bnb/mixin/common/BoatEntityMixin.java
+++ src/main/java/paulevs/bnb/mixin/common/BoatEntityMixin.java
@@ -63,6 +63,8 @@ public abstract class BoatEntityMixin extends Entity {
 	))
 	private void bnb_stopRiding(PlayerEntity player, CallbackInfoReturnable<Boolean> info) {
 		player.setPosition(x, y + 2.5, z);
-		player.setVelocity(velocityX, velocityY, velocityZ);
+		player.velocityX = velocityX;
+		player.velocityY = velocityY;
+		player.velocityZ = velocityZ;
 	}
 }
